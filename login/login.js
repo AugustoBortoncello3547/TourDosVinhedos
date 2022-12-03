d991444c12f3a8cd237356f82915da720b6f9422
Grégori Poloni Machado <gregori.p.m@hotmail.com>
2022-12-03 14:07:46 -0300

    Adiciona template de login e logos

---
+++ login/login.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('formLogin').addEventListener('submit', () => {
+		const email = document.getElementById('emailInput').value;
+		const password = document.getElementById('passwordInput').value;
+
+		/**
+		 * Consulta Fetch/Ajax aqui usando @email e @password
+		 */
+	});
+});
